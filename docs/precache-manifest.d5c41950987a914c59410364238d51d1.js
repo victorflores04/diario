cd40cbf84c19ba58d64961240902d9f86244114b
Victor Flores <flores.vicf@gmail.com>
2020-12-23 09:43:50 -0500

    docs serve

---
+++ docs/precache-manifest.d5c41950987a914c59410364238d51d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162a1804a1cd1cc8b3e2035690dc9826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13b64efb6dc76898f79",
+    "url": "/static/css/main.f1ff31b3.chunk.css"
+  },
+  {
+    "revision": "cc250d20b29b5c16e3ac",
+    "url": "/static/js/2.b83228ad.chunk.js"
+  },
+  {
+    "revision": "4f336ca82fa36f8927f9cc0466d8160c",
+    "url": "/static/js/2.b83228ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13b64efb6dc76898f79",
+    "url": "/static/js/main.52be52d3.chunk.js"
+  },
+  {
+    "revision": "b123e17909d98ae67d72",
+    "url": "/static/js/runtime-main.c6139377.js"
+  },
+  {
+    "revision": "2fee0b5056301e6c2a5bc8df39c37854",
+    "url": "/static/media/background.2fee0b50.png"
+  }
+]);
